4f2b6d03190fc02a1e9dce17ba26ecee23a0b005
Prajakta Charde <Prajakta Charde@DESKTOP-N8HBGCT>
2017-08-23 13:38:57 +0530

    classes Created

---
+++ Githubdemo/Githubdemo/Models/UserMvcContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Githubdemo.Models
+{
+    public class UserMvcContext:DbContext
+    {
+        public UserMvcContext():base("name=MyConnection")
+        {
+
+        }
+        public DbSet<UserMvc> UserMvcs { get; set; }
+    }
+}

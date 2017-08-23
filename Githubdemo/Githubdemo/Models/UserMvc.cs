c0151f4b0c51eb3608e142c8340768670129cb2b
Prajakta Charde <Prajakta Charde@DESKTOP-N8HBGCT>
2017-08-23 13:41:24 +0530

    UserMvc Class Updated

--- Githubdemo/Githubdemo/Models/UserMvc.cs
+++ Githubdemo/Githubdemo/Models/UserMvc.cs
@@ -10,6 +10,7 @@ namespace Githubdemo.Models
         public int id { get; set; }
         public string name { get; set; }
         public int age { get; set; }
-         
+        public string phone { get; set; }
+        public string email { get; set; } 
     }
 }
